00098fb4dcb67a0e77b36d922e386bdd7e0984c2
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-09-19 19:22:52 -0300

    javascript

--- js/script.js
+++ js/script.js
@@ -5,5 +5,47 @@ menuBtn.addEventListener('click', () => {
     [menuBtn, dropdown].forEach(e => {
         e.classList.toggle("ativo");
     })
-    // menuBtn
+})
+
+function initScrollSuave() {
+    const linksInternos = Array.from(document.querySelectorAll('#slide a[href^="#"]'));
+    linksInternos.push(document.getElementById('cta-projeto'));
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    })
+}
+initScrollSuave();
+
+const voltaTopo = document.getElementById("topo");
+voltaTopo.addEventListener('click', () => {
+    let topo = document.body;
+
+    topo.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+})
+
+const emailDiv = document.getElementById('email'),
+    email = 'oliveira.idsoliver23@gmail.com';
+
+emailDiv.addEventListener('click', () => {
+    navigator.clipboard.writeText(email);
+    emailDiv.classList.add('ativo');
+    setTimeout(() => {
+        emailDiv.classList.remove('ativo');
+    }, 1100);
+
 })
